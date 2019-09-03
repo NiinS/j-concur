f93cb0548c3f8a0cc8141f8154e9e8eff60af87f
NitinS <sin.nitins@gmail.com>
2019-09-04 06:17:43 +1000

    f1: new YATAS lock impl.

---
+++ src/main/java/spinlocks/YATASLock.java
@@ -0,0 +1,10 @@
+package spinlocks;
+
+/**
+ * Yet Another Test And Set Lock
+ *
+ * A simple test and set lock with all the baggage of spinning on a single
+ * global lock state field.
+ */
+public class YATASLock {
+}
